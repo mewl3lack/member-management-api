05cecf48923be891fac50e9d936db4c6822e6b75
mewl3lack <musicsitar@hotmail.com>
2021-06-07 21:35:14 +0700

    Adjust api route.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const user = require('./route/user')
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/api', user)
+app.use('/api/employee', user)
 
 app.listen(3000, () => {
     console.log('running at 3000')
