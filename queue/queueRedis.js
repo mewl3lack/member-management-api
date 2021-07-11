5917c082738d71d3e873d1d8f9aa89325b6f0941
mewl3lack <musicsitar@hotmail.com>
2021-07-11 21:31:27 +0700

    Fixed not found queueRedis module.

---
+++ queue/queueRedis.js
@@ -0,0 +1,16 @@
+// var Redis = require('redis')
+// var client = new Redis();
+// var subscriber = new Redis();
+
+// exports.queueOpts = {
+//   createClient: function (type) {
+//     switch (type) {
+//       case 'client':
+//         return client;
+//       case 'subscriber':
+//         return subscriber;
+//       default:
+//         return new Redis();
+//     }
+//   }
+// }
